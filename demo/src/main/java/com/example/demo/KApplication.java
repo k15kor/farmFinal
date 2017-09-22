3fc77bac2f0e5eaf290e27f39f7568edd94549b3
k15kor <k15kor@hotmail.com>
2017-09-22 13:29:54 +0200

    Inicio con Demo

---
+++ demo/src/main/java/com/example/demo/KApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KApplication.class, args);
+	}
+}
